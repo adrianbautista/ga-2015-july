18d8b9e1e954f810c153018f5e3646e25d6ea002
Adrian Bautista <adrianbautista8@gmail.com>
2015-07-14 10:54:15 -0400

    First exercise

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,3 +1,40 @@
 class Bottles
+  def initialize
+  end
 
+  def verse(bottle_count)
+    return_string = "#{bottle_description(bottle_count)} of beer on the wall, #{bottle_description(bottle_count)} of beer.\n"
+
+    return_string += if bottle_count > 1
+      "Take one down and pass it around, " +
+      "#{bottle_description(bottle_count - 1)} of beer on the wall.\n"
+    elsif bottle_count == 1
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    else
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    end
+  end
+
+  def verses(bottle_count_start, bottle_count_end)
+    # ITERATE BACKWARDS OVER RANGE
+    return_string = ""
+    (bottle_count_end..bottle_count_start).reverse_each do |count|
+      return_string += verse(count)
+    end
+    return_string
+  end
+
+  private
+
+  def bottle_description(count)
+    if count > 1
+      "#{count} bottles"
+    elsif count == 1
+      "#{count} bottle"
+    else
+      "no more bottles"
+    end
+  end
 end
---
+++ notes/day-1.md
@@ -0,0 +1,24 @@
+# pairing
+
+- goal can't be moving fast, but instead it is to produce better output that both developers can understand
+- if on weaker side, you have to feel empowered
+
+# themes
+
+- object orientated design
+- refactoring
+	- avoiding the duplication of ideas, not letters
+- testing
+	- if you don't have tests, you can't tell if your refactor worked	
+- misc
+
+===
+
+# naming
+
+- variable naming demonstrates knowledge of the "concept"
+- for single responsibility, method should describe one concept
+- you don't get to use techincal names in method names, use names in the domain of the customer
+- name should be one level higher than implementation
+- messages say what they want, not what they want you to do?
+- 
